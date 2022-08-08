3f52ed2dcc44b3f77077d05e21b4ca441782e712
lyh <lyh@qq.com>
2022-08-08 21:30:23 +0800

    2022.8.8 hot-fix first commit

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -2,8 +2,9 @@ package com.guigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git！！！");
+        System.out.println("hello git1111！！！");
         System.out.println("hello git2222！！！");
         System.out.println("hello git3333！！！");
+        System.out.println("hello git4444！！！");
     }
 }
